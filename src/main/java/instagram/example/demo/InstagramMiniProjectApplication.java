5fc95a605c5b756577fb3bdd603255b353728e28
Parvej697 <pk3580170@gmail.com>
2025-12-14 10:02:54 +0530

    first commit

---
+++ src/main/java/instagram/example/demo/InstagramMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package instagram.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramMiniProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/instagram/example/demo/InstagramMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package instagram.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
